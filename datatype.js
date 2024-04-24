a626cae8b64b2e41788cf1e6b0d7e34b7e0c82a7
ArthPatel007 <arthpatel034@gmail.com>
2024-04-24 18:18:18 +0530

    all files

---
+++ datatype.js
@@ -0,0 +1,12 @@
+"use strict";
+
+const symbol = Symbol('description');
+console.log(typeof symbol); // Output: symbol
+
+let xyz;
+let age=20;
+let name="Arth";
+console.log(typeof "Arth");
+console.log(typeof 20);
+console.log(typeof xyz);
+console.log(typeof null);
--- test.js
+++ test.js
@@ -1,1 +1,10 @@
 console.log("hello");
+
+const accountId=10203;
+let accountMail="raj@gmail.com";
+var accountPasswprd="1236";
+
+// accountId=10;
+
+console.log(accountId);
+console.table([accountId,accountMail,accountPasswprd]);
