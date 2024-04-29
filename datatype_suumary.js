46caf7853c49d3ecfab32d1fb1ed7a2949d6c3c5
ArthPatel007 <arthpatel034@gmail.com>
2024-04-29 14:35:11 +0530

    datatype

---
+++ datatype_suumary.js
@@ -0,0 +1,16 @@
+//Primitiv 
+
+// 7 Type
+// string ,Number,boolearn,Null,undefinde,symbole,BigInt
+const Name="Arth";
+const bigNum="9856322145";
+
+console.log(bigNum);
+// Refrence (Non Primitive)
+
+// Array, Object,Funcation
+
+const heros=["IronMan","Spider-Man","Hulk"];
+let myObj={
+    
+}
