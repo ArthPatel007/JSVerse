f1dea2e22d78c6786461923f2a7daac4fb3677e3
ArthPatel007 <arthpatel034@gmail.com>
2024-05-02 17:05:15 +0530

    all file

---
+++ 06Number and Maths.js
@@ -0,0 +1,27 @@
+const num = 50;
+// console.log(num);
+
+const Balance = new Number(500);
+// console.log(Balance);
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// Maths
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(-5.50));
+// console.log(Math.ceil(5.50));
+// console.log(Math.floor(5.50));
+// console.log(Math.min(1,10,20,30));
+// console.log(Math.max(5,10,20,30));
+
+// console.log(Math.floor(Math.random() * 10) + 1);
+
+// const randomNumber = Math.floor(Math.random() * 6) + 1;
+// console.log(`Random number between 1 to 6: ${randomNumber}`);
+
+const min = 10;
+const max = 30;
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
--- datatype_suumary.js
+++ datatype_suumary.js
@@ -1,21 +1,21 @@
-//Primitiv 
+//Primitiv
 
 // 7 Type
 // string ,Number,boolearn,Null,undefinde,symbole,BigInt
-const Name="Arth";
-const bigNum="9856322145";
+const Name = "Arth";
+const bigNum = "9856322145";
 
 console.log(bigNum);
 // Refrence (Non Primitive)
 
 // Array, Object,Funcation
 
-const heros=["IronMan","Spider-Man","Hulk"];
-let myObj={
-    firstName : "John",
-    lastName  : "Doe",
-    age     : 50,
-    eyeColor  : "blue"
-}
+const heros = ["IronMan", "Spider-Man", "Hulk"];
+let myObj = {
+  firstName: "John",
+  lastName: "Doe",
+  age: 50,
+  eyeColor: "blue",
+};
 
-console.log(heros);
+console.log(typeof heros);
