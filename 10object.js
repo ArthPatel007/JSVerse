814119fcf90a404e1df4311ad76bcb1c9535d9f5
ArthPatel007 <arthpatel034@gmail.com>
2024-05-14 16:33:15 +0530

    ok

--- 10object.js
+++ 10object.js
@@ -1,2 +1,20 @@
 // singleton
+// object.create
 // object literals
+
+const mySym=Symbol("key1");
+
+const user = {
+    "full name": "Arth Patel",
+    age: 20,
+    [mySym]:"mykey1",
+    isLogin: false,
+};
+
+console.log(user.age);
+console.log(user["age"]);
+console.log(user["full name"]);
+console.log(user[mySym]);
+
+user.age=21;
+Object.freeze(user)
