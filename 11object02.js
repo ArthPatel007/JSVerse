a951ec5463a02f74d63a4c43c27279914b3920f2
ArthPatel007 <arthpatel034@gmail.com>
2024-05-20 18:55:32 +0530

    json

--- 11object02.js
+++ 11object02.js
@@ -6,7 +6,7 @@ tinderUser.id = "0456ab";
 tinderUser.name = "Raj";
 tinderUser.isLoggedIn = false;
 
-console.log(tinderUser);
+// console.log(tinderUser);
 
 const regularuser = {
   email: "abc@gmail.com",
@@ -18,7 +18,7 @@ const regularuser = {
   },
 };
 
-console.log(regularuser.fullname.userfullname.firstname);
+// console.log(regularuser.fullname.userfullname.firstname);
 
 const obj1 = { 1: "a", 2: "b" };
 const obj2 = { 3: "a", 4: "b" };
@@ -26,7 +26,7 @@ const obj4 = { 5: "a", 6: "b" };
 
 // const obj3 = { obj1, obj2 };
 const obj3 = Object.assign({}, obj1, obj2, obj4);
-console.log(obj3);
+// console.log(obj3);
 
 const user = [
   {
@@ -40,11 +40,25 @@ const user = [
   {
     id: 3,
     email: "c@gmail.com",
-  }
+  },
 ];
 
-user[1].email
-console.log(tinderUser);
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+user[1].email;
+// console.log(tinderUser);
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
+
+const course = {
+  coursename: "js",
+  price: 999,
+  courseInstorter: "arth",
+};
+
+const { courseInstorter } = course;
+console.log(courseInstorter);
+
+{
+
+}
+
